e143f4733b0327cb2f54cedd321c943a44858e35
Kihun Kim <124813270+kkihoo@users.noreply.github.com>
2024-01-22 17:01:58 +0900

    Title: 행 단위로 반복하여 출력, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/행 단위로 반복하여 출력/print-in-row.py
+++ 240122/행 단위로 반복하여 출력/print-in-row.py
@@ -1,5 +1,5 @@
 n = int(input())
-for i in range(0,n):
-    for j in range(0,n):
-        print(j + 1, end = '')
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(j, end = '')
     print()
