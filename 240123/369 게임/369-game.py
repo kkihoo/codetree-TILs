af87c9ab2371c64a221e0220d33bab66fddea2d2
Kihun Kim <124813270+kkihoo@users.noreply.github.com>
2024-01-23 17:19:36 +0900

    Title: 369 게임, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240123/369 게임/369-game.py
+++ 240123/369 게임/369-game.py
@@ -4,11 +4,11 @@ n = int(input())
 for i in range(1,n+1):
     if i % 3 == 0:
         print(0, end = " ")
-    elif '3' in str(i):
+    elif i == "3":
         print(0, end = " ")
-    elif '6' in str(i):
+    elif i == "6":
         print(0, end = " ")
-    elif '9' in str(i):
+    elif i == "9":
         print(0, end = " ")
     else :
         print(i, end = " ")
