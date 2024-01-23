4071e368d25b41f48b73075171e11950f4143e1c
Kihun Kim <124813270+kkihoo@users.noreply.github.com>
2024-01-23 16:38:03 +0900

    Title: 100점 도달하기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/100점 도달하기/reach-100-point.py
+++ 240123/100점 도달하기/reach-100-point.py
@@ -1,13 +1,13 @@
 n = int(input())
 
-for i in range(n,101):
-    if i < 60:
-        print("F", end = ' ')
-    elif 70 > i >= 60:
-        print("D", end = ' ')
-    elif 80 > i >= 70:
-        print("C", end = ' ')
-    elif 90 > i >= 80:
-        print("B", end = ' ')
-    else:
-        print("A", end = ' ')
+for i in range(n, 101):
+	if i >= 90:
+		print("A", end=" ")
+	elif i >= 80:
+		print("B", end=" ")
+	elif i >= 70:
+		print("C", end=" ")
+	elif i >= 60:
+		print("D", end=" ")
+	else:
+		print("F", end=" ")
