7f529abd49a5a4a979fbd65e430e858026931523
Kihun Kim <124813270+kkihoo@users.noreply.github.com>
2024-01-30 17:32:38 +0900

    Title: 물의 상태, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/물의 상태/state-of-water.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    if (n < 0){
+        cout << "ice" << endl;
+    }
+    else if(n >= 100){
+        cout << "vapor" << endl;
+    }
+    else {
+        cout << "water" << endl;
+    }
+    return 0;
+}
