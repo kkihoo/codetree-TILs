114e53b599c98715b37ba67fc1568b1c31770d0a
Kihun Kim <124813270+kkihoo@users.noreply.github.com>
2024-02-07 15:23:29 +0900

    Title: ab 사이에 없는 c, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240207/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,13 @@
+arr = input().split()
+a, b, c = int(arr[0]), int(arr[1]), int(arr[2])
+
+satis = True
+
+for i in range(a, b + 1):
+    if c % i == 0:
+        satis = False
+
+if satis == False:
+    print("YES")
+else:
+    print("NO")
