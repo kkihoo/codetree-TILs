4ec1cd4045fe40a5bee08b464dd3f8cefd5c1dac
Kihun Kim <124813270+kkihoo@users.noreply.github.com>
2024-02-07 15:49:16 +0900

    Title: 모두 3의 배수, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/모두 3의 배수/all-multiples-of-3.py
+++ 240207/모두 3의 배수/all-multiples-of-3.py
@@ -1,9 +1,10 @@
-satis = False
+satis = True
 
 for i in range(5):
     n = int(input())
-    if n % 3 == 0 :
-        satis = True
+    if n % 3 != 0 :
+        satis = False
+        break
 
 if satis == True:
     print("1")
