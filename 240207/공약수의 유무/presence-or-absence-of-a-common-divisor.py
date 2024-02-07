ff17555d8a70a20f21d5279863d2e9e2728aaeab
Kihun Kim <124813270+kkihoo@users.noreply.github.com>
2024-02-07 15:18:12 +0900

    Title: 공약수의 유무, Time: 156ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240207/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -4,7 +4,7 @@ a, b = int(arr[0]), int(arr[1])
 satis = False
 
 for i in range(a, b + 1):
-    if 1920 % i == 0 or 2880 % i == 0:
+    if 1920 % i == 0 and 2880 % i == 0:
         satis = True
 
 if satis == True:
