dbc93035c4d23c58fc234200410f35d07e87cbbb
Kihun Kim <124813270+kkihoo@users.noreply.github.com>
2024-02-19 18:02:40 +0900

    Title: 별표 출력하기 13, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240219/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(1, n + 1):
+    for _ in range(n - i + 1):
+        print("* ", end="")
+    print()
+    for _ in range(i):
+        print("* ", end="")
+    print()
