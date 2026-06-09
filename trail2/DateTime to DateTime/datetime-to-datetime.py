6da18c89c15823a24d3fbabb7ec2790fab3b438e
Kihun Kim <124813270+kkihoo@users.noreply.github.com>
2026-06-10 00:43:36 +0900

    [Passed] DateTime to DateTime (48ms, 16MB)

--- trail2/DateTime to DateTime/datetime-to-datetime.py
+++ trail2/DateTime to DateTime/datetime-to-datetime.py
@@ -21,3 +21,12 @@ else:
             day += 1
             hour = 0
     print(elapsed_times)
+"""
+a, b, c = map(int, input().split())
+
+start = 10*24*60 + 11*60 + 11
+
+end = (a-1)*24*60 + b*60 + c
+
+print(end - start) if end >= start else print(-1)
+"""
